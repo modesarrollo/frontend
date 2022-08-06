43c2f0d7f04f86a1032067f11c54c951a4171717
modesarrollo <105142502+modesarrollo@users.noreply.github.com>
2022-08-06 18:48:16 -0500

    Update script.js

--- landingpage-evento-veeam/script.js
+++ landingpage-evento-veeam/script.js
@@ -23,3 +23,72 @@ var parallaxInstance = new Parallax(scene);
 var scene2 = document.getElementById('scene2');
 var parallaxInstance = new Parallax(scene2);
 AOS.init();
+
+function SendFormGoogleSheets() {
+    var nombre=document.getElementById('entry.1562361540').value;
+    var celular=document.getElementById('entry.1935066067').value;
+    var email=document.getElementById('entry.1520008568').value;
+    var empresa=document.getElementById('entry.1454788060').value;
+    var cargo=document.getElementById('entry.1337371359').value;
+
+    if(nombre==""){
+        Swal.fire({
+                text: 'El nombre es obligatorio digitarlo',
+                icon: 'warning',
+                confirmButtonText: 'Aceptar',
+                backdrop: true,
+                timer: 3000,
+                toast: 'true',
+                position: 'center',
+            });
+    document.elementById("entry.1562361540").focus();
+    }
+    if(celular==""){
+        Swal.fire({
+                text: 'El número del celular es obligatorio digitarlo',
+                icon: 'warning',
+                confirmButtonText: 'Aceptar',
+                backdrop: true,
+                timer: 3000,
+                toast: 'true',
+                position: 'center',
+            });
+    document.elementById("entry.1935066067").focus();
+    }
+    if(email==""){
+        Swal.fire({
+                text: 'El correo electrónico es obligatorio digitarlo',
+                icon: 'warning',
+                confirmButtonText: 'Aceptar',
+                backdrop: true,
+                timer: 3000,
+                toast: 'true',
+                position: 'center',
+            });
+    document.elementById("entry.1520008568").focus();
+    }
+    if(empresa==""){
+        Swal.fire({
+                text: 'El nombre de la empresa es obligatorio digitarlo',
+                icon: 'warning',
+                confirmButtonText: 'Aceptar',
+                backdrop: true,
+                timer: 3000,
+                toast: 'true',
+                position: 'center',
+            });
+    document.elementById("entry.1454788060").focus();
+    }
+    if(cargo==""){
+        Swal.fire({
+                text: 'El cargo es obligatorio digitarlo',
+                icon: 'warning',
+                confirmButtonText: 'Aceptar',
+                backdrop: true,
+                timer: 3000,
+                toast: 'true',
+                position: 'center',
+            });
+    document.elementById("entry.1337371359").focus();
+    }
+}
